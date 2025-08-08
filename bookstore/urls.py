a1c2f171c68446a85158f06d6bbea434f081fd7c
Faizan Nadeem <89235471+faizannadeem422@users.noreply.github.com>
2025-08-08 11:49:08 +0500

    Initial Commit

---
+++ bookstore/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import AddBook, DeleteBook, ViewBook, FetchAllBooks, FetchBooksByCategory, UpdateBook, Home
+
+urlpatterns = [
+    # path('home/', Home, name='home'),
+    path('add/', AddBook, name='add-book'),
+    path('id/<int:bookId>/', ViewBook, name='view-book'),
+    path('all', FetchAllBooks, name='fetch-all-books'),
+    path('fetchbycategory/<str:category_p>/', FetchBooksByCategory, name='fetch-books-by-category'),
+    path('update/<int:bookId>', UpdateBook, name='update-a-book'),
+    path('delete/<int:bookId>', DeleteBook, name='delete-book')
+]
